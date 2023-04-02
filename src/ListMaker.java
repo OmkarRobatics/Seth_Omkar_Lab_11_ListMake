147b2bf129819fa80de85bf1523905c1f48f4bae
OmkarRobatics <omkarseth008@gmail.com>
2023-04-02 11:58:17 -0400

    Project Init

--- src/ListMaker.java
+++ src/ListMaker.java
@@ -2,82 +2,74 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ListMaker {
-  private static  ArrayList<String> list =  new ArrayList<>();
+    private static final ArrayList<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
+            // var declarations
+        final String menu = "A - Add D - Delete P - Print Q - Quit"; // menu options
+        boolean done = false;
+        String itemAdded = "";
+        int deletingItem = 0;
+        boolean validItemNumber = false; // valid item number to delete
+        String command = "";
+        Scanner in = new Scanner(System.in);
+        DisplayList();
 
-        final String menu = "A - Add D - Delete P - Print Q - Quit";
-                boolean done = false;
-                String itemAdded = "";
-                int deletingItem = 0;
-                boolean validItem = false;
-                String command = "";
-                Scanner in = new Scanner(System.in);
-                DisplayList();
+        do {
 
-                do
-                {
+            command = SafeInput.getRegexString(in, menu, "[AaDdPpQq]");
+            command = command.toUpperCase();
+            switch (command) {
+                case "A":
+                    itemAdded = SafeInput.getNonZeroLenString(in, "Enter a item to add to the list: ");
+                    list.add(itemAdded);
+                    DisplayList();
+                    break;
+                case "D":
+                    if (list.size() == 0) {
+                        System.out.println("You can not delete anything from the list because it is empty. Add something first.");
+                    } else {
+                        deletingItem = SafeInput.getInt(in, "Enter the number of the item you would like to delete on the list");
 
-                    command = SafeInput.getRegexString(in,menu,"[AaDdPpQq]");
-                    command = command.toUpperCase();
-                    switch (command)
-                    {
-                        case "A":
-                        itemAdded = SafeInput.getNonZeroLenString(in,"Enter a item to add to the list: ");
-                            list.add(itemAdded);
-                            DisplayList();
-                            break;
-                            case "D":
-                                if (list.size() == 0)
-                                {
-                                    System.out.println("You can not delete anything from the list because it is empty. Add something first.");
+                        if (list.size() >= deletingItem && deletingItem > 0) {
+                            deletingItem = deletingItem - 1;
+                            list.remove(deletingItem);
+                        } else {
+                            do {
+                                System.out.println("You entered an invalid number: " + deletingItem + "." + " Please try again.");
+                                deletingItem = SafeInput.getInt(in, "Enter the number of the item you would like to delete on the list");
+
+                                if (list.size() >= deletingItem && deletingItem > 0) {
+                                    deletingItem = deletingItem - 1;
+                                    list.remove(deletingItem);
+                                    validItemNumber = true;
                                 }
-                                else {
-                                    deletingItem = SafeInput.getInt(in, "Enter the number of the item you would like to delete on the list");
+                            } while (!validItemNumber);
 
-                                    if (list.size() >= deletingItem && deletingItem > 0)
-                                    {
-                                        deletingItem = deletingItem - 1;
-                                        list.remove(deletingItem);
-                                    }
-                                    else
-                                    {
-                                        do {
-                                            System.out.println("You entered an invalid number: " + deletingItem + "." + " Please try again.");
-                                            deletingItem = SafeInput.getInt(in, "Enter the number of the item you would like to delete on the list");
-
-                                            if (list.size() >= deletingItem && deletingItem > 0)
-                                            {
-                                                deletingItem = deletingItem - 1;
-                                                list.remove(deletingItem);
-                                                validItem = true;
-                                            }
-                                        }while (!validItem);
-
-                                    }
-                                }
-                            break;
-                        case "P":
-                            DisplayList();
-                            break;
-                        case "Q":
-                            System.exit(0);
-                            break;
+                        }
                     }
-                }while (!done);
+                    break;
+                case "P":
+                    DisplayList();
+                    break;
+                case "Q":
+                    System.exit(0);
+                    break;
             }
-            private static void DisplayList () {
-                        System.out.println("*****************************************************");
-                if (list.size() != 0) {
-                    for (int index = 0; index < list.size(); index++)
-                    {
-                        System.out.printf("\n%3d%35s", index + 1, list.get(index));
-                    }
-                }
-                    else {
-                        System.out.println("***             No Items in List                  ***");
-                    }
-                        System.out.println("\n*****************************************************");
+        } while (!done);
+    }
 
+    private static void DisplayList() {
+        System.out.println("*****************************************************");
+        if (list.size() != 0) {
+            for (int index = 0; index < list.size(); index++) {
+                System.out.printf("\n%3d%35s", index + 1, list.get(index));
             }
-
+        } else {
+            System.out.println("***             No Items in List                  ***");
         }
+        System.out.println("\n*****************************************************");
+
+    }
+
+}
